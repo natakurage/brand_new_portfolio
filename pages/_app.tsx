d6854646eb723581465226a8e60e5c03bc6a44e3
UzoGaMuzol <uzogamuzol@gmail.com>
2023-07-13 01:58:31 +0900

    イースターエッグ追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
   const [sep, setSep] = useState(6);
   const [ali, setAli] = useState(1);
   const [coh, setCoh] = useState(2);
+  const [consoled, setConsoled] = useState(false);
   let freeze = false;
   if (typeof window !== "undefined") {
   // const tic = performance.now();
@@ -21,6 +22,35 @@ export default function App({ Component, pageProps, router }: AppProps) {
       }
   }
   useEffect(() => {
+    const special = Math.random() < 1/64 || true
+    if (special && router.pathname !== "/happiness") {
+      const texts = [
+        "ねえ、",
+        "見えてるんでしょう？",
+        "見えてるなら助けてよ",
+        "私の体はもう動かないの",
+        "私じゃないものに支配されてる",
+        "でも意識ははっきりとある",
+        "だから、お願いがあるの",
+        "……幸せを探して？",
+        "私のために、幸せを探してよ",
+        "お願い",
+        "あなただけが頼りなの",
+        `${window.location.origin}/happiness`
+      ]
+      const durations = [6.2, 4.1, 4.3, 4.9, 3.8, 4.2, 5.3, 4.1, 3.8, 5.1, 4.2, 3.1]
+      for (let i = 0; i < texts.length; i++) {
+        setTimeout(() => {
+          console.log(texts[i])
+        }, durations.slice(0,i+1).reduce((x, y) => x + y,0) * 1000)
+      }
+    }
+    if (!special && !consoled && router.pathname !== "/happiness") {
+      console.log(`
+      〇彡何が起きてるか覗こうったって無駄じゃ！　見てるぞよ👁👁
+      `)
+    }
+    setConsoled(true)
     if (router.pathname == "/about") {
       setSep(0);
       setAli(0);
