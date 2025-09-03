71d8003d5201b1753943c57085d91432b20afa2c
natakurage <natakurage@natakurage.cc>
2025-09-03 10:57:31 +0900

    マウス位置を修正

--- src/components/CursorCircleMask.tsx
+++ src/components/CursorCircleMask.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function CursorCircleMask(
   { radius = 200, attenuation = 100, className }
   : { radius?: number, attenuation?: number, className?: string }
 ) {
+  const ref = useRef<HTMLDivElement>(null);
   const [mousePosition, setMousePosition] = useState({ x: -1000, y: -1000 });
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
+      const rect = ref.current?.getBoundingClientRect();
+      setMousePosition({ 
+        x: event.clientX - (rect?.left ?? 0), 
+        y: event.clientY - (rect?.top ?? 0) 
+      });
     };
     window.addEventListener("mousemove", handleMouseMove);
     return () => {
@@ -20,6 +25,7 @@ export function CursorCircleMask(
   
   return (
     <div
+      ref={ref}
       className={`absolute inset-0 ${className ?? ""}`}
       style={{
         maskImage: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, transparent ${radius}px, black ${radius + attenuation}px)`,
